83e9f4d060423e7b1a6cf80d5e9ab81c9e282af4
Bekzat Tilekbayl <tlekbay.b@gmail.com>
2022-03-20 13:20:44 +0000

    Frontend: display results in /experiments

--- src/components/SlideThumbnails.js
+++ src/components/SlideThumbnails.js
@@ -3,12 +3,24 @@ import React from 'react';
 const WIDTH = 500;
 const HEIGHT = WIDTH * 9 / 16;
 
+function randomColor() {
+    return Math.random().toString(16).substring(2, 8);
+}
+
 function SlideThumbnails(props) {
     const presentationId = props?.presentationId;
     const slideInfo = props?.slideInfo;
 
     const startIdx = props?.startIdx;
     const endIdx = props?.endIdx;
+
+    const outline = props?.outline;
+    let colors = [];
+    if (outline) {
+        for (let i = 0; i < outline.length; i++) {
+            colors.push(randomColor());
+        }
+    }
 
     const output = slideInfo?.map((slide, idx) => {
         if (startIdx && startIdx > idx) {
@@ -27,8 +39,22 @@ function SlideThumbnails(props) {
         const endTime = new Date(0);
         endTime.setSeconds(slide.endTime);
 
+        // Outline
+        let curSectionTitleIdx = -1;
+
+        if (outline) {
+            for (let i = 0; i < outline.length; i++) {
+                if (outline[i].startSlideIndex <= idx && outline[i].endSlideIndex >= idx) {
+                    curSectionTitleIdx = i;
+                }
+            }
+        }
+
         return (
             <div key={idx} 
+                style={{
+                    backgroundColor: (curSectionTitleIdx < 0 ? null : '#' + colors[curSectionTitleIdx])
+                }}
             >
                 <div style={{
                     overflow: "hidden",
@@ -46,7 +72,8 @@ function SlideThumbnails(props) {
                 </div>
                 <div> 
                     {idx} {" "}
-                    ({startTime.getMinutes()}:{startTime.getSeconds()} - {endTime.getMinutes()}:{endTime.getSeconds()})
+                    ({startTime.getMinutes()}:{startTime.getSeconds()} - {endTime.getMinutes()}:{endTime.getSeconds()}) {" "}
+                    {curSectionTitleIdx < 0 ? null : outline[curSectionTitleIdx].sectionTitle}
                 </div>	
             </div>
         )
--- src/pages/Experiments.js
+++ src/pages/Experiments.js
@@ -2,48 +2,43 @@ import axios from 'axios';
 import { useEffect, useState } from "react";
 import SlideThumbnails from '../components/SlideThumbnails';
 
-function Experiments() {
-    const startPresentationId = 0;
-    const endPresentationId = 100;
+const INTERVAL = 10;
+const PRESENTATION_IDS_CNT = 745;
 
+function Experiments() {
     const [allPresentations, setAllPresentations] = useState([])
+    const [showAllSlides, setShowAllSlides] = useState(false);
+    const [startPresentationId, setStartPresentationId] = useState(0);
+    const [endPresentationId, setEndPresentationId] = useState(INTERVAL);
 
     useEffect(() => {
-        let requests = []
+        let presentationIds = []
         for (let presentationId = startPresentationId; presentationId < endPresentationId; presentationId++) {
-            const similarityType = "keywords";
-            const similarityMethod = "tf-idf";
-            const outliningApproach = "dp_mask";
-            const applyThresholding = true;
-            const applyHeuristics = true;
-            requests.push(axios.post('http://server.hyungyu.com:7777/mapping/process_presentation', {
-                presentationId: presentationId,
-                similarityType: similarityType,
-                similarityMethod: similarityMethod,
-                outliningApproach: outliningApproach,
-                applyThresholding: applyThresholding,
-                applyHeuristics: applyHeuristics,
-            }));
+            presentationIds.push(presentationId);
         }
-        Promise.all(requests).then( (responses) => {
+        axios.post('http://server.hyungyu.com:7777/mapping/bulk_data', {
+            presentationIds: presentationIds
+        }).then( (response) => {
+            console.log(response);
+            const bulkData = response.data.bulkData;
             let _allPresentations = []
-            for (let response of responses) {
-                _allPresentations.push(response.data);
+            for (let presentationData of bulkData) {
+                _allPresentations.push(presentationData);
             }
             setAllPresentations(_allPresentations);
         });
-    }, [])
+    }, [startPresentationId, endPresentationId])
     
     const outputKRandomPerPresentation = (k) => {
         let output = []
         for (let presentationId = startPresentationId; presentationId < endPresentationId; presentationId++) {
             let row = [];
-            for (let id = 0; id < k+2; id++) {
+            for (let id = -1; id < k+2; id++) {
                 //const rid = Math.round(Math.random() * 8) + 1;
                 const rid = id + 1;
                 let curLink = `http://server.hyungyu.com:7777/images/${presentationId}/${rid}.jpg`;
                 
-                if (id === k) {
+                if (id === -1) {
                     curLink = `http://server.hyungyu.com:7777/images/${presentationId}/acc_frame_0.jpg`;    
                 }
                 else if (id === k+1) {
@@ -77,13 +72,7 @@ function Experiments() {
         return allPresentations.map((allData, allIdx) => {
             const presentationId = allData?.presentationId;
             const data = allData?.data;
-            const script = allData?.script;
             let slideInfo = data?.slideInfo ? data.slideInfo : [];
-
-            for (let idx = 0; idx < slideInfo.length; idx++) {
-                slideInfo[idx].script = script[idx];
-            }
-            console.log(slideInfo); 
             return <div key={allIdx} style={{
                 margin: "1em",
             }}>
@@ -91,16 +80,74 @@ function Experiments() {
                 <SlideThumbnails
                     slideInfo={slideInfo}
                     presentationId={presentationId}
-                    startIdx={0}
+                    startIdx={1}
                     endIdx={data?.slideCnt}
+                    outline={data?.outline}
                 />
             </div>
         })
     }
 
+    const outputSelectorRanges = (interval) => {
+        let output = []
+        for (let presentationId = 0; presentationId < PRESENTATION_IDS_CNT; presentationId += interval) {
+            const start = presentationId;
+            const end = Math.min(PRESENTATION_IDS_CNT, presentationId + interval);
+            output.push(
+                <option
+                    value={start.toString() + "-" + end.toString()}
+                    key={start.toString() + "-" + end.toString()}
+                >
+                    {start} - {end - 1}
+                </option>
+            );
+        }
+        return output
+    }
+
     return (<div>
-        {outputKRandomPerPresentation(4)}
-        {/* { outputPresentations() } */}
+        <div style={{
+            margin: "1em",
+        }}>
+            <label
+                htmlFor="presentationIds"
+            > Show All Slides </label>
+            <select 
+                id="presentationIds"
+                value={
+                    startPresentationId.toString()+'-'+endPresentationId.toString()
+                }
+                onChange={(event) => {
+                    const value = event.target.value;
+                    const ids = value.split("-").map((val) => Number.parseInt(val));
+                    setStartPresentationId(ids[0])
+                    setEndPresentationId(ids[1])
+                }}
+            >
+                {
+                    outputSelectorRanges(INTERVAL)
+                }
+            </select>
+        </div>
+        <div style={{
+            margin: "1em",
+        }}>
+            <label
+                htmlFor="showAllSlides"
+            > Show All Slides </label>
+            <input 
+                id="showAllSlides"
+                type="checkbox"
+                value={showAllSlides}
+                onChange={(event) => setShowAllSlides(event.target.checked)}
+            />
+        </div>
+        {
+            showAllSlides ?
+            outputPresentations()
+            :
+            outputKRandomPerPresentation(4)
+        }
     </div>)
 
 }
